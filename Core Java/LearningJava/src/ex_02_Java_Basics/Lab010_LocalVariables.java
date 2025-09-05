fda4aff094252fef1d9c8150e0ef67cdc77ce7d5
prapti3 <prapti.chavan3@gmail.com>
2025-09-05 16:40:54 +0530

    code update

---
+++ Core Java/LearningJava/src/ex_02_Java_Basics/Lab010_LocalVariables.java
@@ -0,0 +1,27 @@
+package ex_02_Java_Basics;
+
+public class Lab010_LocalVariables {
+    public static void main(String[] args) {
+
+        int a = 10;
+        byte b = 20;
+        char c = 'f';
+        short d = 40;
+        long l = 50L;
+        float f = 60.5f;
+        double db = 4353.4;
+        boolean bool = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(db);
+        System.out.println(bool);
+
+
+
+    }
+}
