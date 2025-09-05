3712643f067fbe136dd5c58a355a74d7d26cf68b
prapti3 <prapti.chavan3@gmail.com>
2025-09-05 15:17:26 +0530

    Keywords and identifiers

---
+++ Core Java/LearningJava/src/ex_01_Java_Basics/Lab002_Comments.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab002_Comments {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        // Single line comment
+        /* Multi line comments */
+
+
+
+    }
+}
---
+++ Core Java/LearningJava/src/ex_01_Java_Basics/Lab003_Keywords_and_Identifiers.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basics;
+
+public class Lab003_Keywords_and_Identifiers {
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
