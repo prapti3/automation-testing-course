324cb26cb09023230775fdf5b8c0b21c0f3385f0
prapti3 <prapti.chavan3@gmail.com>
2025-09-05 19:53:26 +0530

    code update

--- Core Java/LearningJava/src/ex_04_Operator/Lab013_New_Operators.java
+++ Core Java/LearningJava/src/ex_04_Operator/Lab013_New_Operators.java
@@ -1,4 +1,11 @@
 package ex_04_Operator;
 
 public class Lab013_New_Operators {
+    public static void main(String[] args) {
+        //new operator - oops
+        String name = "Pramod";
+//      String name = new String("Pramod");
+
+
+    }
 }
---
+++ Core Java/LearningJava/src/ex_04_Operator/Lab014_InstanceOf_Operator.java
@@ -0,0 +1,13 @@
+package ex_04_Operator;
+
+public class Lab014_InstanceOf_Operator {
+    public static void main(String[] args) {
+        String s1 = new String("Hello");
+        System.out.println(s1 instanceof String);
+        System.out.println(s1 instanceof Object);
+//        System.out.println(s1 instanceof Integer);
+
+
+
+    }
+}
