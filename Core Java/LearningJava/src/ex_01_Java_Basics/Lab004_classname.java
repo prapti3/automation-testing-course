cd0d3fa21beea6de348b4700d20a2c19bd5a7841
prapti3 <prapti.chavan3@gmail.com>
2025-09-05 15:42:14 +0530

    Vaariables and datatypes

---
+++ Core Java/LearningJava/src/ex_01_Java_Basics/Lab004_classname.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basics;
+
+public class Lab004_classname {
+    public static void main(String[] args) {
+
+    }
+}
+/*
+ * abc$abc
+ * 123Avc
+ * _abc
+ * abc_Abc
+ *
+ */
+
---
+++ Core Java/LearningJava/src/ex_02_Java_Basics/Lab001_VariablesDatatypes.java
@@ -0,0 +1,39 @@
+package ex_02_Java_Basics;
+
+public class Lab001_VariablesDatatypes {
+    public static void main(String[] args) {
+//        variables  - ```
+//data_type variable_name =  variable_value
+//```
+
+        /*
+        * types
+        * 1. Primitive Types
+        * 2. Non-Primitive  Types , (Reference)
+        *
+        *
+        *
+        *
+ * 1. Primitive Types - That can't be broken further.
+
+          * 1. Boolean Type -> boolean -> true or false.
+          * 2. Numeric Type-Intergral
+            *  1. byte
+            *  2. short
+            *  3. int
+            *  4. long
+            *  5. char
+         * 3. Floating
+            * 1. float
+            * 2. double
+*2. Reference or Non Primitive  Types
+         * 1. class, interface, enum, String
+
+
+        * */
+
+
+
+
+    }
+}
